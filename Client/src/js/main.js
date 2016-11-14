0edfecf8462a275d78cf82222bbf92ab882fe187
Rahul-Tiwari96 <rahuldit2012@gmail.com>
2016-11-14 07:17:58 +0000

    First Commit Editing

---
+++ Client/src/js/main.js
@@ -0,0 +1,25 @@
+var React=require('react');
+var ReactDOM=require('react-dom');
+var Route=require('react-router').Route;
+var Router=require('react-router').Router;
+var {browserHistory}= require ('react-router');
+var IndexRoute=require('react-router').IndexRoute;
+var hashHistory=require('react-router').hashHistory;
+var LoginComponent=require('./Components/LoginComponent');
+var MainComponent=require('./Components/MainComponent');
+var About=require('./Components/About');
+var ParentComponent=require('./Components/ParentComponent');
+var Examples=require('./Components/Examples');
+var GetFavouriteRepositories=require('./Components/GetFavouriteRepositories');
+
+ReactDOM.render(
+	<Router history={browserHistory}>
+		<Route path="/" component={MainComponent}>
+		<Route path="LoginComponent" component={LoginComponent}></Route>
+		<Route path="about" component={About}></Route>
+		<Route path="examples" component={Examples}></Route>
+		<Route path="getFavourites" component={GetFavouriteRepositories}></Route>
+		<IndexRoute component={ParentComponent}></IndexRoute>
+		</Route>
+	</Router>,
+	document.getElementById('app')); //puts the virtual dom & injects into the main physical DOM.
